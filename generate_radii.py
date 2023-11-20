6bc3ad9fc43d8e7f12c0f2dceb10813bde7f6516
agkgd4 <kellerandreag@gmail.com>
2023-11-20 09:46:50 -0600

    generating new radii

--- generate_radii.py
+++ generate_radii.py
@@ -2,20 +2,16 @@ import numpy as np
 import yaml
 from IPython import embed
 import pickle
+import matplotlib.pyplot as plt
 
-if __name__=="__main__":
+def get_radii(params):
 
-    params = yaml.load(open("config.yaml", 'r'), Loader = yaml.FullLoader)
     list_len = 9
     
-    library = []
-    
-    #idx 0 - 2, uniform grids 
-    #library.append([params['geometry']['radius_min'] for _ in range(list_len)]) # uniform, min rad
-    #library.append([(params['geometry']['radius_max']/2) for _ in range(list_len)]) # uniform, central rad
-    #library.append([params['geometry']['radius_max'] for _ in range(list_len)]) # uniform, max rad
-
-    # idx 5 through 15, random with small variation
+    library = {} 
+    library['std_dev'] = []
+    library['percent_of_range'] = []
+    library['radii'] = [] 
 
     rows, cols = 3, 3
    
@@ -23,23 +19,69 @@ if __name__=="__main__":
     _min = params['geometry']['radius_min']
     _max = params['geometry']['radius_max']
     
-    while i < 10: 
+    while i < 30: 
 
         initial_value = params['geometry']['radius_max'] / 2 
-        std_dev = 0.005 * (i + 1)
-        print(std_dev)
- 
-        radii = [initial_value + np.random.normal(0, std_dev) for _ in range(9)]
+        std_dev = 0.0025 * (i + 1)
+        library['std_dev'].append(std_dev)
+        library['percent_of_range'].append((std_dev / (0.250-0.075)) * 100)
 
-        radii = np.clip(radii, _min, _max)
-        
-        # Ensure no duplicates in the sublist
-        while len(set(radii)) < len(radii):
+        for j in range(10):
+            if j == 0:
+                rad_list = []
+
             radii = [initial_value + np.random.normal(0, std_dev) for _ in range(9)]
-            radii = np.clip(radii, _min, _max)
 
-        # Append the generated sublist to the list of lists
-        library.append(list(radii))
-        i += 1 
-    pickle.dump(library, open("buffer_study_library.pkl", "wb"))
+            radii = np.clip(radii, _min, _max)
+            
+            # Ensure no duplicates in the sublist
+            while len(set(radii)) < len(radii):
+                radii = [initial_value + np.random.normal(0, std_dev) for _ in range(9)]
+                radii = np.clip(radii, _min, _max)
+
+            # Append the generated sublist to the list of lists
+            rad_list.append(list(radii))
+            
+            if j == 9:
+                library['radii'].append(rad_list)
+                print(f"appended to library['radii']: list length = {len(rad_list)}")
+        i += 1
+         
+    #pickle.dump(library, open("buffer_study_library.pkl", "wb"))
+    return library
+
+def visualize(library):
+
+    std_dev = library['std_dev']
+    percent_of_range = library['percent_of_range']
+    radii = library['radii']
+    
+    #fig, ax = plt.subplots(2, int(len(radii[0])/2), figsize=(8,8))
+    fig, ax = plt.subplots(1, int(len(radii)))
+    #embed() 
+    for i, (rad_list, sd) in enumerate(zip(radii,std_dev)):
+        std_dev = np.std(rad_list, ddof=1)
+        print(f"np calculated: {std_dev}, compare to {sd} - error = {np.abs(std_dev-sd)}") 
+       
+        ax[i] = plt.boxplot(rad_list) 
+        
+        
+        
+        
+        
+    plt.show()
+def rad_inc_with_y(params):
+
+   pass 
+ 
+if __name__=="__main__":
+
+    params = yaml.load(open("config.yaml", 'r'), Loader = yaml.FullLoader)
+
+    expected_val = params['geometry']['radius_max'] / 2
+    library = get_radii(params)
+
+    visualize(library)
+    #pickle.dump(library, open("buffer_study_random.pkl","wb"))
     #embed()
+
