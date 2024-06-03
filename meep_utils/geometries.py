b0b613e2fda799f89deaa77b09a20c39ff099aee
agkgd4 <kellerandreag@gmail.com>
2024-06-03 14:53:48 -0500

    getting rid of some logger outputs

--- meep_utils/geometries.py
+++ meep_utils/geometries.py
@@ -13,9 +13,9 @@ from loguru import logger
 ##############################################################################
 
 def build_cylinder(loc:list, axis:list, height:float, radius:float, material_index:float) -> mp.Cylinder:
-    logger.info("Building a MEEP cylinder")
+    #logger.info("Building a MEEP cylinder")
 
-    logger.info("Creating cylinder material. Index = {}".format(material_index))
+    #logger.info("Creating cylinder material. Index = {}".format(material_index))
     material = mp.Medium(index = material_index)
     center = mp.Vector3(loc[0], loc[1], loc[2])
     axis = mp.Vector3(axis[0], axis[1], axis[2])
@@ -27,9 +27,9 @@ def build_cylinder(loc:list, axis:list, height:float, radius:float, material_ind
                         material = material )
 
 def build_block(size:list, loc:list, material_index:float) -> mp.Block:
-    logger.info("Building a MEEP block")
+    #logger.info("Building a MEEP block")
 
-    logger.info("Creating block material. Index = {}".format(material_index))
+    #logger.info("Creating block material. Index = {}".format(material_index))
     material = mp.Medium(index = material_index)
     size = mp.Vector3(size[0], size[1], size[2])
     center = mp.Vector3(loc[0], loc[1], loc[2])
@@ -46,7 +46,7 @@ def build_block(size:list, loc:list, material_index:float) -> mp.Block:
 
 def build_silica_pdms_substrate(params:dict) -> list:
 
-    logger.info("Building silica + PDMS substrate.")
+    #logger.info("Building silica + PDMS substrate.")
     ##################################
     #     _____________________
     #    |        PDMS         |
@@ -56,7 +56,7 @@ def build_silica_pdms_substrate(params:dict) -> list:
     #
     ##################################
 
-    logger.info("Reading fused silica parameters")
+    #logger.info("Reading fused silica parameters")
 
     size_x_buffer = params['size_x_buffer']
     size_y_buffer = params['size_y_buffer']
@@ -75,15 +75,15 @@ def build_silica_pdms_substrate(params:dict) -> list:
     loc_z_fused_silica = params['loc_z_fused_silica']
     material_index_fused_silica = params['material_index_fused_silica']
 
-    logger.info("Creating fused silica material. Index = {}".format(material_index_fused_silica))
+    #logger.info("Creating fused silica material. Index = {}".format(material_index_fused_silica))
 
     fused_silica = build_block( size = [size_x_fused_silica, size_y_fused_silica, size_z_fused_silica],
                                 loc = [loc_x_fused_silica, loc_y_fused_silica, loc_z_fused_silica],
                                 material_index = material_index_fused_silica)
-    logger.info("Fused silica size: {}".format(fused_silica.size))
-    logger.info("Fused silica center: {}".format(fused_silica.center))
+    #logger.info("Fused silica size: {}".format(fused_silica.size))
+    #logger.info("Fused silica center: {}".format(fused_silica.center))
 
-    logger.info("Reading PDMS parameters")
+    #logger.info("Reading PDMS parameters")
     loc_x_pdms = params['loc_x_pdms'] + offset_x_buffer
     loc_y_pdms = params['loc_y_pdms'] + offset_y_buffer
     loc_z_pdms = params['loc_z_pdms']
@@ -94,14 +94,14 @@ def build_silica_pdms_substrate(params:dict) -> list:
 
     material_index_pdms = params['material_index_pdms']
 
-    logger.info("Creating fused silica material. Index = {}".format(material_index_fused_silica))
+    #logger.info("Creating fused silica material. Index = {}".format(material_index_fused_silica))
 
     pdms = build_block( size = [size_x_pdms, size_y_pdms, size_z_pdms],
                         loc = [loc_x_pdms, loc_y_pdms, loc_z_pdms],
                         material_index = material_index_pdms )
 
-    logger.info("PDMS size : {}".format(pdms.size))
-    logger.info("PDMS loc : {}".format(pdms.center))
+    #logger.info("PDMS size : {}".format(pdms.size))
+    #logger.info("PDMS loc : {}".format(pdms.center))
     
     return [fused_silica, pdms]
 
@@ -114,30 +114,30 @@ def build_andy_metasurface_neighborhood(params, radii = None):
     '''
     geometry_params = params['geometry']
     Nx, Ny = geometry_params['neighborhood_size']
-    logger.info("Creating metasurface neighborhood. Nx,Ny : {},{}".format(Nx,Ny))
+    #logger.info("Creating metasurface neighborhood. Nx,Ny : {},{}".format(Nx,Ny))
 
     atom_type = geometry_params['atom_type']
-    logger.info("Meta-atom type: {}".format(atom_type))
+    #logger.info("Meta-atom type: {}".format(atom_type))
 
     unit_cell_size = geometry_params['unit_cell_size']
-    logger.info("Meta-atom size: {}".format(unit_cell_size))
+    #logger.info("Meta-atom size: {}".format(unit_cell_size))
 
     if geometry_params['substrate_buffer']:
-        logger.info("Buffer is enabled")
+        #logger.info("Buffer is enabled")
         size_x_buffer = geometry_params['size_x_buffer']
         size_y_buffer = geometry_params['size_y_buffer']
         size_z_buffer = geometry_params['size_z_buffer']
-        logger.info("Buffer : {}, {}, {}".format(size_x_buffer, size_y_buffer, size_z_buffer))
+        #logger.info("Buffer : {}, {}, {}".format(size_x_buffer, size_y_buffer, size_z_buffer))
 
         offset_x_buffer = round(size_x_buffer / 2, 4)
         offset_y_buffer = round(size_y_buffer / 2, 4)
         offset_z_buffer = round(size_z_buffer / 2, 4)
-        logger.info("Center offset added by buffer : {}, {}, {}".format(offset_x_buffer, offset_y_buffer, offset_z_buffer))
+        #logger.info("Center offset added by buffer : {}, {}, {}".format(offset_x_buffer, offset_y_buffer, offset_z_buffer))
         params['geometry']['offset_x_buffer'] = offset_x_buffer
         params['geometry']['offset_y_buffer'] = offset_y_buffer
         params['geometry']['offset_z_buffer'] = offset_z_buffer
     else:
-        logger.info("Buffer is disabled")
+        #logger.info("Buffer is disabled")
         size_x_buffer = 0
         size_y_buffer = 0
         size_z_buffer = 0
@@ -163,7 +163,7 @@ def build_andy_metasurface_neighborhood(params, radii = None):
     params['cell_y'] = size_y_cell
     params['cell_z'] = size_z_cell
 
-    logger.info("Size of total geometry cell : {} x {} x {} [um]".format(size_x_cell, size_y_cell, size_z_cell))
+    #logger.info("Size of total geometry cell : {} x {} x {} [um]".format(size_x_cell, size_y_cell, size_z_cell))
     cell_size = mp.Vector3(size_x_cell, size_y_cell, size_z_cell)
     params['geometry']['cell_size'] = cell_size
 
@@ -174,31 +174,31 @@ def build_andy_metasurface_neighborhood(params, radii = None):
 
 
     loc_z_fused_silica = round(0.5 * size_z_fused_silica - 0.5 * size_z_cell,4)
-    logger.info("Center Z of fused silica : {} [um]".format(loc_z_fused_silica))
+    #logger.info("Center Z of fused silica : {} [um]".format(loc_z_fused_silica))
     loc_z_pdms = round(size_z_fused_silica + 0.5 * size_z_pdms - 0.5 * size_z_cell, 4)
-    logger.info("Center Z of pdms : {} [um]".format(loc_z_pdms))
+    #logger.info("Center Z of pdms : {} [um]".format(loc_z_pdms))
     loc_z_pillar = round(size_z_fused_silica + 0.5 * height_pillar - 0.5 * size_z_cell, 4)
 
-    logger.info("Center Z of pillars : {} [um]".format(loc_z_pillar))
+    #logger.info("Center Z of pillars : {} [um]".format(loc_z_pillar))
 
     params['geometry']['loc_z_fused_silica'] = loc_z_fused_silica
     params['geometry']['loc_z_pdms'] = loc_z_pdms
     params['geometry']['loc_z_pillar'] = loc_z_pillar
 
     loc_top_fused_silica = round(size_z_fused_silica - 0.5 * size_z_cell, 4)
-    logger.info("Top of the fused silica : {} [um]".format(loc_top_fused_silica))
+    #logger.info("Top of the fused silica : {} [um]".format(loc_top_fused_silica))
 
     #The top of the pdms - the whole size minus the amount in the PML
     loc_top_pdms = round(size_z_fused_silica + (size_z_pdms - thickness_pml) - 0.5 * size_z_cell, 4)
-    logger.info("Top of the pdms : {} [um]".format(loc_top_pdms))
+    #logger.info("Top of the pdms : {} [um]".format(loc_top_pdms))
 
-    logger.info("Updating params with calculated locations")
+    #logger.info("Updating params with calculated locations")
     params['geometry']['loc_top_fused_silica'] = loc_top_fused_silica
     params['geometry']['loc_top_pdms'] = loc_top_pdms
 
     #Get the size of the non pml region
     size_z_non_pml = size_z_fused_silica + size_z_pdms - 2*thickness_pml
-    logger.info("Size of the non PML volume : {}".format(size_z_non_pml))
+    #logger.info("Size of the non PML volume : {}".format(size_z_non_pml))
 
     #Get the center of the simulation cell
     #loc_z_center_cell = 0 
@@ -206,14 +206,14 @@ def build_andy_metasurface_neighborhood(params, radii = None):
     loc_x_center_cell = 0
     loc_y_center_cell = 0
     center_sim_cell = mp.Vector3(loc_x_center_cell, loc_y_center_cell, loc_z_center_cell)
-    logger.info("Center of the simulation cell : {}".format(center_sim_cell))
+    #logger.info("Center of the simulation cell : {}".format(center_sim_cell))
     params['geometry']['center_sim_cell'] = center_sim_cell
 
     size_z_non_pml 
     #Get the size of the nonbuffer region
     size_x_non_buffer = params['geometry']['unit_cell_size'] * params['grid_size']
     size_y_non_buffer = params['geometry']['unit_cell_size'] * params['grid_size']
-    logger.info("Size of the non buffer volume : x: {} y: {}".format(size_x_non_buffer, size_y_non_buffer))
+    #logger.info("Size of the non buffer volume : x: {} y: {}".format(size_x_non_buffer, size_y_non_buffer))
 
     params['geometry']['size_z_non_pml'] = size_z_non_pml
     params['geometry']['size_x_non_buffer'] = size_x_non_buffer
@@ -258,7 +258,7 @@ def build_andy_metasurface_neighborhood(params, radii = None):
     if radii == None:
         radii = [0.2 for _ in range(0,Nx*Ny)]
 
-    logger.info("Radii of the pillars : {}".format(radii))
+    #logger.info("Radii of the pillars : {}".format(radii))
     count = 0
     for ny in range(0,Ny):
         for nx in range(0,Nx):
